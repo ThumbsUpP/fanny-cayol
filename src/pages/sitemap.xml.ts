ccc830ddb8724a51cc8f79a98f1f4d56be5faf09
Pierre CAYOL <pierre.cayol@gmail.com>
2026-09-02 15:56:48 +0200

    seo: improve crawl discovery and brand signals

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,11 +1,27 @@
 import type { APIRoute } from 'astro';
 
-const paths = ['/', '/mentions-legales/', '/politique-confidentialite/'];
+const paths = [
+  '/',
+  '/osteopathie/',
+  '/osteopathie-femme/',
+  '/osteopathie-pediatrique/',
+  '/drainage-lymphatique/',
+  '/massages/',
+  '/fanny-cayol-peridon/',
+  '/cabinet-tarifs-rendez-vous/',
+  '/mentions-legales/',
+  '/politique-confidentialite/',
+];
+
+const lastModified = '2026-09-02';
 
 export const GET: APIRoute = ({ site }) => {
   const baseUrl = site ?? new URL('https://www.fanny-cayol-peridon.fr');
   const urls = paths
-    .map((path) => `  <url><loc>${new URL(path, baseUrl).toString()}</loc></url>`)
+    .map(
+      (path) =>
+        `  <url>\n    <loc>${new URL(path, baseUrl).toString()}</loc>\n    <lastmod>${lastModified}</lastmod>\n  </url>`,
+    )
     .join('\n');
 
   const body = `<?xml version="1.0" encoding="UTF-8"?>
